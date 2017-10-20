e05d224f85c5221e044ce0602fc2885924b16c38
PlutoMa <mademaomail@126.com>
2017-10-20 15:00:20 +0800

    Begin VideoSplash

---
+++ 014.VideoSplash/VideoSplash/VideoSplash/VideoCutter.swift
@@ -0,0 +1,20 @@
+//
+//  VideoCutter.swift
+//  VideoSplash
+//
+//  Created by Dareway on 2017/10/20.
+//  Copyright © 2017年 Pluto. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+extension String {
+    var convert: NSString { return self as NSString }
+}
+
+class VideoCutter {
+    func cropVideoWithUrl(videoUrl url: NSURL, startTime) -> <#return type#> {
+        <#function body#>
+    }
+}
---
+++ 014.VideoSplash/VideoSplash/VideoSplash/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  VideoSplash
+//
+//  Created by Dareway on 2017/10/20.
+//  Copyright © 2017年 Pluto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
