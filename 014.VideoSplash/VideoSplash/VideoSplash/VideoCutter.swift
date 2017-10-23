b661329b1e489f0c6529b9af6a8a7ec03cb19ece
PlutoMa <mademaomail@126.com>
2017-10-23 14:21:55 +0800

    continue VideoSplash

--- 014.VideoSplash/VideoSplash/VideoSplash/VideoCutter.swift
+++ 014.VideoSplash/VideoSplash/VideoSplash/VideoCutter.swift
@@ -14,7 +14,19 @@ extension String {
 }
 
 class VideoCutter {
-    func cropVideoWithUrl(videoUrl url: NSURL, startTime) -> <#return type#> {
-        <#function body#>
+    func cropVideoWithUrl(videoUrl url: NSURL, startTime: CGFloat, duration: CGFloat, completion: ((_ videoPath: NSURL?, _ error: NSError?) -> Void)?) -> Void {
+        DispatchQueue.global().async {
+            let asset = AVURLAsset(url: url as URL, options: nil)
+            let exportSession = AVAssetExportSession(asset: asset, presetName: "AVAssetExportPresetHighestQuality")
+            let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+            var outputPath = paths.last!
+            let fileManager = FileManager.default
+            try? fileManager.createDirectory(atPath: outputPath, withIntermediateDirectories: true, attributes: nil)
+            outputPath = outputPath.convert.appendingPathComponent("output.mp4")
+            try? fileManager.removeItem(atPath: outputPath)
+            if let exportSession = exportSession as AVAssetExportSession? {
+                
+            }
+        }
     }
 }
